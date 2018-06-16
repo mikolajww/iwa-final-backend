6e1754bf3aab77fa597c72b09da2e8b0db3b2914
IWA-3 <iwa-3@se.dmcs.pl>
2018-06-16 19:31:39 +0200

    Repositories added Authorization Contoller added

--- src/main/java/pl/dmcs/mww/repository/GradeRepository.java
+++ src/main/java/pl/dmcs/mww/repository/GradeRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.dmcs.mww.model.Grade;
-import pl.dmcs.mww.model.Student;
 
 @Repository
 @CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
